788321b6b4f27040f1cdf8f5a57bcfd707928220
evandersondev <evandersonvasconcelos23@gmail.com>
2020-05-22 15:11:11 -0300

    updating config heroku

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+    'no-console': 'off',
+    'no-unused-vars': 'off',
+    'react/prop-types': [0],
+    'guard-for-in': 'error',
+    'no-restricted-syntax': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'no-await-in-loop': 'off',
+  },
+};
