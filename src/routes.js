ff98ef2b7996105bc7a848b9001642bf0c8235c2
evandersondev <evandersonvasconcelos23@gmail.com>
2020-05-26 17:38:37 -0300

    deleted netlify.toml and updating routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Info from './pages/Info';
@@ -9,8 +9,9 @@ export default () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/info/:name" component={Info} />
-      <Route path="*" component={NotFound} />
+      <Route path="/info/:name" component={Info} />
+      <Route path="/404" component={NotFound} />
+      <Redirect to="/404" />
     </Switch>
   </BrowserRouter>
 );
