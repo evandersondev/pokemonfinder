bffbbd41bb5f403e9689d723e8b3bfb8e105f090
evandersondev <evandersonvasconcelos23@gmail.com>
2020-05-26 11:24:24 -0300

    deleted __tests__

--- src/pages/Home/index.spec.js
+++ src/pages/Home/index.spec.js
@@ -1,37 +1,18 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { shallow } from 'enzyme';
-import api from '../../services/api';
 
 import Home from '.';
 
-jest.mock('../../services/api', () => {
-  return {
-    getAllPaginated({ perPage, page = 1 }) {
-      return {
-        results: [{ name: 'spearow' }],
-        previous: null,
-        next: 'url/qualquer',
-      };
-    },
-  };
-});
-
 describe('test render components in Home', () => {
-  it('renders corretly', () => {
-    const wrapper = shallow(<Home />);
-
-    expect(wrapper).toMatchSnapshot();
-  });
-
   it('should have two links', () => {
-    const wrapper = shallow(<Home />);
+    const wrapper = shallow(<Home pokemonName="Pikachu" />);
 
     expect(wrapper.find('Link').length).toBe(2);
   });
 
   it('should a Link with name Previous and Next', () => {
-    const wrapper = shallow(<Home />);
+    const wrapper = shallow(<Home pokemonName="Pikachu" />);
 
     const text = wrapper.find('Link').map((node) => node.text());
     expect(text).toEqual(['Previous', 'Next']);
